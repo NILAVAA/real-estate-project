c12c3ddf74165585fecf0e8219b110b004a47d7e
nilava <banerjeenil027@gmail.com>
2024-05-08 12:07:11 +0530

    part -5

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,15 @@ export default {
         xl:"1.5rem",
         "2xl":"1.75rem",
         "3xl":"2.25rem",
+      },
+      backgroundImage: {
+        'team-bg-image': "url('/public/team.png')",
+        'team-bol': "url('/public/public/b.png.jpg)",
+        
       }
-    },
   },
+},
   plugins: [],
-}
 
+
+}
